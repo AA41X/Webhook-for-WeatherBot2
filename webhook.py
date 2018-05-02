6eade988788fca114e809262c41d5275f3af6452
AA41X <34483033+AA41X@users.noreply.github.com>
2018-05-02 12:06:43 +0200

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -45,10 +45,12 @@ def makeResponse(req):
     "source": "dialogflow-weather-webhook"
     }
 
+"""
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
+"""
 
 
 
